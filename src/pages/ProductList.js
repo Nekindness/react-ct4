3c4f53969b84bcb170b99a044a40a26fab237201
Nekindness <137820039+Nekindness@users.noreply.github.com>
2024-04-25 16:13:47 +0300

    ProductList.js

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -2,14 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import StarRating from "./StarRating";
 
-// Компонент для отображения списка товаров
 function ProductList() {
-  // Используем хук useState для хранения состояния списка товаров
   const [products, setProducts] = useState([]);
-
-  // Используем хук useEffect для выполнения API-запроса при загрузке компонента
   useEffect(() => {
-    // Определяем асинхронную функцию fetchShop для выполнения API-запроса
     const fetchShop = async () => {
       const response = await fetch(`https://fakestoreapi.com/products/`);
       const data = await response.json();
@@ -18,11 +13,9 @@ function ProductList() {
     fetchShop();
   }, []);
 
-  // Возвращаем JSX для отображения списка товаров
   return (
     <div className="product-list-container">
       <ul className="product-list">
-        {/* Маппим каждый товар на элемент списка с ссылкой на страницу товара и рейтингом */}
         {products.map((product) => (
           <li key={product.id} className="product-item">
             <Link to={`/products/${product.id}`} className="product-link">
