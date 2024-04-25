7a46abe8e167c67f85f0d4adca2bd094c1f2d4ca
Nekindness <137820039+Nekindness@users.noreply.github.com>
2024-04-25 16:11:48 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ import ProductDetail from "./pages/ProductDetail";
 import NotFoundPage from "./pages/NotFoundPage";
 import "./App.css";
 
-// Функция компонента App, отвечающая за маршрутизацию в приложении
 function App() {
   return (
-    // Обертка Router с указанием базового пути
     <Router basename="/RECTLESS-CP-4">
-      {/* Определение маршрутов */}
       <Routes>
-        {/* Маршрут для отображения списка всех товаров */}
         <Route path="/products" element={<ProductList />} />
-        {/* Маршрут для отображения подробной информации о товаре с динамическим параметром productId */}
         <Route path="/products/:productId" element={<ProductDetail />} />
-        {/* Маршрут для отображения страницы NotFound в случае неизвестного маршрута */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
